76b7f0b3296f4ddd0bdd9f99c4c6d7df17c46713
Luis M. Franco <127321871+luismfranco@users.noreply.github.com>
2024-08-16 12:19:41 -0700

    Add files via upload

--- automatedMouseMaze.py
+++ automatedMouseMaze.py
@@ -1,10 +1,27 @@
+
+"""
+Modules
+
+"""
+
+import sys; sys.path.append("src")
 import json
 import tkinter as tk
 import mazeGUI
 
+"""
+Configuration
+
+"""
+
 f = open("config/package.json")
 configurationData = json.load(f)
 f.close()
+
+"""
+Run Application
+
+"""
 
 mainWindow = tk.Tk()
 mazeGUI.mazeGUI(mainWindow,configurationData)
